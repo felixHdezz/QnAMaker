99f7b94aede43856bab97029718c01953dd1f5ff
FelixHdezz <felix_hdezz95@hotmail.com>
2018-11-07 17:02:11 -0600

    upload files fisrt commit

---
+++ QnAMakerResult.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Bot.Sample.QnABot
+{
+    [Serializable]
+    public class QnAMakerResults
+    {
+        public QnAMakerResults() { }
+
+        public QnAMakerResults(List<QnAMakerResult> qnaMakerResults)
+        {
+            Answers = qnaMakerResults;
+        }
+
+        [JsonProperty(PropertyName = "answers")]
+        public List<QnAMakerResult> Answers { get; set; }
+
+        internal QnAMakerAttribute ServiceCfg { get; set; }
+    }
+
+    [Serializable]
+    public class QnAMakerResult
+    {
+        public QnAMakerResult() { }
+
+        public QnAMakerResult(string answer, List<string> questions, double score)
+        {
+            Answer = answer;
+            Questions = questions;
+            Score = score;
+        }
+
+        [JsonProperty(PropertyName = "questions")]
+        public List<string> Questions { get; set; }
+
+        [JsonProperty(PropertyName = "answer")]
+        public string Answer { get; set; }
+
+        [JsonProperty(PropertyName = "score")]
+        public double Score { get; set; }
+    }
+}
