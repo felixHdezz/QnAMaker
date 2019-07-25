c27e52c2ebf6fb5cba3180db785c3ef9e49c6a68
FelixHdezz <felix_hdezz95@hotmail.com>
2019-07-24 21:45:44 -0500

    models

---
+++ Models/TResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QnABot.Models
+{
+    public class TResponseModel
+    {
+        public string Reponse { get; set; }
+
+        public bool Error { get; set; } = false;
+
+        public string Exception { get; set; }
+
+        public string MessageError { get; set; }
+    }
+}
